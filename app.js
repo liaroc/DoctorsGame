676b853fe0392d7252b05f42012e3b50443efc0f
mourdjen <liaroc13@gmail.com>
2019-08-30 18:21:43 +0200

    make clicking match questions + rename index and style

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+// var text = ["main","pied","tete","genou"];
+////var x = Math.floor((Math.random()*2));
+// $('#myTxt').html(text[x]);//
+// document.getElementById("myTxt").innerHTML = "Paragraph changed!";
+var r_text = new Array();
+
+r_text = [
+          ["trouver l'epaule", "E"], 
+          ["trouver la main", "M"], 
+          ["trouver le coude", "C"], 
+          ["trouver la hanche", "H"],
+          ["trouver le pied", "P"],
+          ["trouver le genoux", "G"]
+        ]
+
+
+var randomQuestion = Math.floor(6 * Math.random());
+console.log(randomQuestion);
+document.write(r_text[randomQuestion][0]);
+
+var blabla = r_text[1][1];
+
+
+
+
 function myFunction() {
   var circles = document.getElementsByClassName('circle');
   for (let circle of circles) {
@@ -6,29 +31,12 @@ function myFunction() {
         circle.classList.remove('red');
       }
       circle.classList.add('red');
-      if (circle.classList.contains('E')) {
-        document.getElementsByClassName('shoulder')[0].classList.remove('text');
+      if (circle.classList.contains(r_text[randomQuestion][1])) {
+        document.getElementsByClassName('winningState')[0].classList.remove('text');
+        circle.classList.add('green');
       }
     });
   }
 }
 
-// var text = ["main","pied","tete","genou"];
-////var x = Math.floor((Math.random()*2));
-// $('#myTxt').html(text[x]);//
-// document.getElementById("myTxt").innerHTML = "Paragraph changed!";
-var r_text = new Array();
-r_text[0] = "trouver l'epaule";
-r_text[1] = 'trouver l main';
-r_text[2] = 'trouver la jambe';
-r_text[3] = 'trouver';
-r_text[4] = "I'd be safe and warm";
-r_text[5] = 'If I was in L.A.';
-r_text[6] = "California dreaming, On such a winter's day";
-
-var i = Math.random();
-
-i = 6 * i;
-i = Math.floor(i);
-
-var i = Math.floor(7 * Math.random());
+myFunction();
